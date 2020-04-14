f8c9269885167feb471c0d3c3e3642f330473a2e
fgrunewald <f.grunewald@rug.nl>
2020-04-14 16:23:02 +0200

    working structure generator

--- polyply/src/backmap.py
+++ polyply/src/backmap.py
@@ -1,39 +1,23 @@
+import numpy as np
 from .processor import Processor
-import subprocess
+
 
 class Backmap(Processor):
-    """
 
-    """
-    backward_options = [
-    "-f",          # Input  GRO/PDB structure
-    "-o",          # Output GRO/PDB structure
-    "-raw",        # Projected structure before geometric modifications
-    "-n",          # Output NDX index file with default groups
-    "-p",          # Atomistic target topology
-    "-po",         # Output target topology with matching molecules list
-    "-pp",         # Processed target topology, with resolved #includes
-    "-atomlist",   # Atomlist according to target topology
-    "-fc",         # Position restraint force constant
-    "-to",         # Output force field
-    "-from",       # Input force field
-    "-strict",     # Use strict format for PDB files
-    "-nt",         # Use neutral termini for proteins
-    "-sol",        # Write water
-    "-solname",    # Residue name for solvent molecules
-    "-kick",       # Random kick added to output atom positions
-    "-nopbc",      # Don't try to unbreak residues (like when having large residues in a small box)
-    "-mapdir", ]   # Directory where to look for the mapping files
 
-    def _prepare_input(self, meta_molecule):
-        
-
-    def _run_backwards(self, meta_molecule):
-
-        process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-
+    def _place_init_coords(self, meta_molecule):
+        coords = np.zeros((len(meta_molecule.molecule.nodes), 3))
+        idx = 0
+        for node in meta_molecule.nodes:
+            resname = meta_molecule.nodes[node]["resname"]
+            CoG = meta_molecule.nodes[node]["position"]
+            template = meta_molecule.templates[resname]
+            for atom, vector in template.items():
+                new_coords = CoG + vector
+                coords[idx:(idx+1), :] = new_coords
+                idx += 1
+        meta_molecule.coords = coords
 
     def run_molecule(self, meta_molecule):
-        self._random_walk(meta_molecule)
+        self._place_init_coords(meta_molecule)
         return meta_molecule
