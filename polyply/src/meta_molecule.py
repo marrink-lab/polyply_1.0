f0e737f9ef53eeaa3ec5ddae5dbafa280f4859b0
fgrunewald <f.grunewald@rug.nl>
2020-04-15 13:18:00 +0200

    add itp parser for meta_molecule

--- polyply/src/meta_molecule.py
+++ polyply/src/meta_molecule.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import json
 import networkx as nx
 from networkx.readwrite import json_graph
+from polyply.src.parsers import read_polyply
 
 Monomer = namedtuple('Monomer', 'resname, n_blocks')
 
