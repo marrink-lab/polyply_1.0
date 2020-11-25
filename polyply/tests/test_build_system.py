c8fb8c04534890f8c208d7d807b35c84337ab8e6
fgrunewald <f.grunewald@rug.nl>
2020-11-25 18:02:42 +0100

    rework and test build system

--- polyply/tests/test_build_system.py
+++ polyply/tests/test_build_system.py
@@ -14,28 +14,25 @@
 """
 Test that force field files are properly read.
 """
-import math
 import pytest
+import textwrap
 import numpy as np
-from numpy.linalg import norm
-import vermouth
+from vermouth.forcefield import ForceField
 import polyply
-from polyply import TEST_DATA
-from polyply.src.meta_molecule import Monomer, MetaMolecule
 from polyply.src.topology import Topology
-from polyply.src.nonbond_matrix import NonBondMatrix
+from polyply.src.top_parser import read_topology
 from polyply.src.build_system import (_compute_box_size,
-                                      _filter_by_molname
+                                      _filter_by_molname,
+                                      BuildSystem
                                      )
 
-
 @pytest.fixture
-def topology():
+def test_topology():
     top_lines ="""
     [ defaults ]
     1   1   no   1.0     1.0
     [ atomtypes ]
-    N0 72.0 0.000 A 0.0 0.0
+    N0 45.0 0.000 A 0.0 0.0
     [ nonbond_params ]
     N0   N0   1 4.700000e-01    3.700000e+00
     [ moleculetype ]
@@ -56,9 +53,9 @@ def topology():
     [ moleculetype ]
     testB 1
     [ atoms ]
-    1    N0  1   ASP    BB   1 0.00     45
-    2    N0  1   ASP    SC1  1 0.00     45
-    3    N0  1   ASP    SC2  1 0.00     45
+    1    N0  1   ASP    BB   1 0.00
+    2    N0  1   ASP    SC1  1 0.00
+    3    N0  1   ASP    SC2  1 0.00
     [ bonds ]
     1    2    1  0.47 2000
     2    3    1  0.47 2000
@@ -79,54 +76,148 @@ def topology():
 
 @pytest.mark.parametrize('density, result', (
     (1000.0,
-     4.2119903964305125
-     ),
+     1.0387661572640916
+    ),
     (600.0,
-     4.993866813213379
+     1.2315934632345062
      )))
-def test_compute_box_size(density, result):
-    top = create_topology(100)
+def test_compute_box_size(test_topology, density, result):
+    top = test_topology
+    print(_compute_box_size(top, density))
     assert np.isclose(_compute_box_size(top, density), result)
 
-
-
 @pytest.mark.parametrize('ignore', (
-    ["NA"],
-    ["AA", "NA"],
+    ["test_A"],
+    ["test_A", "test_B"],
      ))
-def test_filer_by_molname(test_system, ignore):
-    molecules = _filter_by_molname(test_system.molecules, ignore)
+def test_filer_by_molname(test_topology, ignore):
+    molecules = _filter_by_molname(test_topology.molecules, ignore)
     for molecule in molecules:
         assert molecule.mol_name not in ignore
 
 
-@pytest.mark.parametrize('ignore', (
-      density,
-      start_dict,
-      max_force=10**3,
-      grid_spacing=0.2,
-      maxiter=800,
-      maxiter_random=50,
-      box=[],
-      step_fudge=1,
-      push=[],
-      ignore=[],
-      grid=None):
+@pytest.mark.parametrize('box, density, grid_spacing, grid, expected_grid_shape, expected_box', (
+   # grid and box are generated automatically
+   (None,
+    1000,
+    0.2,
+    None,
+    (216, 3),
+    np.array([1.03877, 1.03877, 1.03877])
+   ),
+   # box is provided and grid is generated automatically
+   (np.array([5.0, 5.0, 5.0]),
+    1000,
+    0.2,
+    None,
+    (15625, 3),
+    np.array([5.0, 5.0, 5.0]),
+   ),
+   # box is provided and grid also nothing is generated
+   (np.array([5.0, 5.0, 5.0]),
+    1000,
+    0.2,
+    np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]),
+    (2, 3),
+    np.array([5.0, 5.0, 5.0]),
+   ),
+   ))
+def test_init(test_topology,
+             box, density,
+             grid_spacing, grid,
+             expected_grid_shape,
+             expected_box):
 
-def test_run_molecule(test_system, defaults):
-   # iterate over
-   # density vs box
-   # grid_spacind vs grid
-   # start and ignore
-   BuildSystem(test_system,
-               start_dict=start_dict,
-               density=args.density,
-               max_force=args.max_force,
-               grid_spacing=args.grid_spacing,
-               maxiter=args.maxiter,
-               maxiter_random=args.maxiter_random,
-               box=box,
-               step_fudge=args.step_fudge,
-               push=args.push,
-               ignore=args.ignore,
-               grid=grid).run_system(topology.molecules)
+   processor = BuildSystem(test_topology,
+                           start_dict={},
+                           box=box,
+                           density=density,
+                           grid=grid,
+                           grid_spacing=grid_spacing,
+                           )
+   assert processor.box_grid.shape == expected_grid_shape
+   assert all(processor.box == expected_box)
+
+def test_rw_arg_failure():
+    with pytest.raises(TypeError):
+         BuildSystem(test_topology,
+                     start_dict={},
+                     random_argument=10)
+
+@pytest.mark.parametrize("""positions, box,
+                          starting_grid,
+                          starting_nodes,
+                          """, (
+   # no inital starting positoins
+   ([],
+    np.array([5., 5., 5.]),
+    np.array([[2.0, 2.0, 2.0],
+              [3.0, 3.0, 3.0],
+              [0.5, 0.5, 0.5],
+              [4.0, 4.0, 4.0]]),
+    {0:0, 1:0, 2:0}
+   ),
+   # skip first molecule
+   (np.array([[1.25, 1.00, 1.00],
+              [1.75, 1.00, 1.00],
+             ]),
+    np.array([5., 5., 5.]),
+    np.array([[2.0, 2.0, 2.0],
+              [3.0, 3.0, 3.0],
+              [0.5, 0.5, 0.5],
+              [4.0, 4.0, 4.0]]),
+   {0:0, 1:0, 2:0}
+   ),
+   # partial starting positions
+   (np.array([[1.25, 1.00, 1.00]]),
+    np.array([5., 5., 5.]),
+    np.array([[2.0, 2.0, 2.0],
+              [3.0, 3.0, 3.0],
+              [0.5, 0.5, 0.5],
+              [4.0, 4.0, 4.0]]),
+    {0:0, 1:0, 2:0}
+   ),
+   # scramble starting nodes
+   ([],
+    np.array([5., 5., 5.]),
+    np.array([[2.0, 2.0, 2.0],
+              [3.0, 3.0, 3.0],
+              [0.5, 0.5, 0.5],
+              [4.0, 4.0, 4.0]]),
+    {0:1, 1:0, 2:0}
+   ),
+   ))
+def test_build_system(positions,
+                      test_topology,
+                      box,
+                      starting_grid,
+                      starting_nodes):
+
+   total = 0
+   for mol in test_topology.molecules:
+       for node in mol.nodes:
+           try:
+               mol.nodes[node]["position"] = positions[total]
+               mol.nodes[node]["build"] = False
+           except IndexError:
+               mol.nodes[node]["build"] = True
+           total += 1
+
+
+   processor = BuildSystem(test_topology,
+                           density=1000,
+                           start_dict=starting_nodes,
+                           box=box,
+                           grid=starting_grid,
+                           )
+   processor.run_system(test_topology)
+   total = 0
+   for mol_idx, mol in enumerate(test_topology.molecules):
+       for node in mol.nodes:
+           if total < len(positions):
+              assert all(mol.nodes[node]["position"] == positions[total])
+           elif node == starting_nodes[mol_idx]:
+              assert mol.nodes[node]["position"] in starting_grid
+           else:
+              assert "position" in mol.nodes[node]
+           total += 1
