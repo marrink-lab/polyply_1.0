e52bd0ae9a184ef9c45cee03c22ff24dd952652f
fgrunewald <32294573+fgrunewald@users.noreply.github.com>
2021-01-25 18:17:15 +0100

    Update setup.py
    get the version dynamically from the file repro. Setting it in the cfg file doesn't work for some reason, because the PBR call seems to be executed not within the source repro. It's a mess.

--- setup.py
+++ setup.py
@@ -13,7 +13,22 @@
 # limitations under the License.
 
 import os
+import os.path
 from setuptools import find_packages, setup
+import codecs
+# this comes form here https://packaging.python.org/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 def package_files(directory):
     paths = []
@@ -27,4 +42,5 @@ setup(
                   + package_files('polyply/tests/test_data'),},
     scripts=['bin/polyply'],
     pbr=True,
+    version=get_version("package/__init__.py")
 )
