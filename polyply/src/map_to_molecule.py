912879530f1c71076ae0f2fdb55ab0a041c0ced6
fgrunewald <f.grunewald@rug.nl>
2021-03-24 15:35:55 +0100

    improve map to molecule to recognize proteins etc. draft

--- polyply/src/map_to_molecule.py
+++ polyply/src/map_to_molecule.py
@@ -13,12 +13,9 @@
 # limitations under the License.
 
 import networkx as nx
-from vermouth.graph_utils import (make_residue_graph,
-                                  collect_residues)
 from polyply.src.processor import Processor
-from polyply.src.graph_utils import is_branched
 
-def tag_exclusions(blocks, force_field):
+def tag_exclusions(node_to_block, force_field):
     """
     Given the names of some `blocks` check if the
     corresponding molecules in `force_field` have
@@ -30,14 +27,39 @@ def tag_exclusions(blocks, force_field):
     Note the tag is picked up by apply links where
     the excluions are generated.
     """
-    excls = [force_field.blocks[mol].nrexcl for mol in blocks]
+    excls = {}
+    for node in node_to_block:
+        block = force_field.blocks[node_to_block[node]]
+        excls[node] = block.nrexcl
 
-    if len(set(excls)) > 1:
-        min_excl = min(excls)
-        for excl, mol in zip(excls, blocks):
-            block = force_field.blocks[mol]
+    if len(set(excls.values())) > 1:
+        min_excl = min(list(excls.values()))
+        for node, excl in excls.items():
+            block = force_field.blocks[node_to_block[node]]
             nx.set_node_attributes(block, excl, "exclude")
             block.nrexcl = min_excl
+
+def _correspondence_to_residue(meta_molecule,
+                               molecule,
+                               correspondence,
+                               res_node):
+    """
+    """
+    print(meta_molecule.nodes[res_node])
+    resid = meta_molecule.nodes[res_node]["resid"]
+    residue = nx.Graph()
+    for mol_node in correspondence.values():
+        data = molecule.nodes[mol_node]
+        if data["resid"] == resid:
+            residue.add_node(mol_node, **data)
+            for attribute, value in meta_molecule.nodes[res_node].items():
+                # graph and seqID are specific attributes set by make residue
+                # graph or the gen_seq tool, which we don't want to propagate.
+                if attribute in ["graph", "seqID"]:
+                    continue
+                residue.nodes[mol_node][attribute] = value
+
+    return residue
 
 class MapToMolecule(Processor):
     """
@@ -48,47 +70,52 @@ class MapToMolecule(Processor):
     single meta molecule or a system. The later is currently not
     implemented.
     """
-    @staticmethod
-    def expand_meta_graph(meta_molecule, block, meta_mol_node):
+    def __init__(self, force_field):
+        self.node_to_block = {}
+        self.node_to_fragment = {}
+        self.fragments = []
+        self.multiblock_correspondence = []
+        self.added_fragments = []
+        self.added_fragment_nodes = []
+        self.force_field = force_field
+
+    def match_nodes_to_blocks(self, meta_molecule):
         """
-        When a multiresidue block is encounterd the individual
-        residues are added instead of the orignial block to
-        the meta molecule.
+        For all nodes in the meta_molecule match them either to a
+        block in the force_field or to a block that is a multi
+        residue molecule, in which case the node has the restart
+        attribute. There is a third option in which a resname matches
+        a block, which is itself a multiresidue block.
         """
-        # 0. make edges for the block
-        for inter_type in ["bonds", "constraints", "virtual_sitesn",
-                           "virtual_sites2", "virtual_sites3", "virtual_sites4"]:
-            block.make_edges_from_interaction_type(inter_type)
+        regular_graph = nx.Graph()
+        restart_graph = nx.Graph()
+        restart_attr = nx.get_node_attributes(meta_molecule, "from_itp")
 
-        # 1. make residue graph
-        expanded_graph = make_residue_graph(block)
+        # this breaks down when to proteins are directly linked
+        # but that is an edge-case we can worry about later
+        for idx, jdx in nx.dfs_edges(meta_molecule):
+            # the two nodes are restart nodes
+            if idx in restart_attr and jdx in restart_attr:
+                restart_graph.add_edge(idx, jdx)
+            else:
+                regular_graph.add_edge(idx, jdx)
 
-        # 2. clear out all old edges, because the
-        # inserted part will not have any edges with the
-        # rest of the meta_molecule as we don't know those
-        old_edges = list(meta_molecule.edges(meta_mol_node))
-        meta_molecule.remove_edges_from(old_edges)
+        # regular nodes have to mactch a block in the force-field by resname
+        for node in regular_graph.nodes:
+            self.node_to_block[node] = meta_molecule.nodes[node]["resname"]
 
-        # 3. relable nodes to make space for new nodes to be inserted
-        mapping = {}
-        offset = len(expanded_graph.nodes) - 1
-        for node in meta_molecule.nodes:
-            if node > meta_mol_node:
-                mapping[node] = node + offset
-
-        nx.relabel_nodes(meta_molecule, mapping, copy=False)
-        # 4. add the new nodes to the meta molecule overwriting
-        # the inital nodes inserting the graph
-        nodes = sorted(expanded_graph.nodes)
-        for node in nodes:
-            node_key = node + meta_mol_node
-            attrs = expanded_graph.nodes[node]
-            attrs["links"] = False
-            meta_molecule.add_node(node_key, **attrs)
-
-        # 5. add all edges within the exapnded block
-        for edge in expanded_graph.edges:
-            meta_molecule.add_edge(edge[0] + meta_mol_node, edge[1] + meta_mol_node)
+        # restart nodes are match a block in the force-field but are themselves
+        # only part of that block
+        for fragment in nx.connected_components(restart_graph):
+            block_name = restart_attr[list(fragment.nodes())[0]]
+            if all([restart_attr[node] == block_name  for node in fragment.nodes]):
+                self.fragments.append(fragment)
+                block = self.force_field.blocks[block_name]
+                for node in fragment.nodes:
+                    self.node_to_block[node] = block_name
+                    self.node_to_fragment[node] = len(self.fragments) - 1
+            else:
+                raise IOError
 
     def add_blocks(self, meta_molecule):
         """
@@ -96,50 +123,64 @@ class MapToMolecule(Processor):
         and if a multiresidue block is encountered expand the meta
         molecule graph to include the block at residue level.
         """
-        force_field = meta_molecule.force_field
-        resnames = set(nx.get_node_attributes(meta_molecule, "resname").values())
-        tag_exclusions(resnames, force_field)
-
-        block = force_field.blocks[meta_molecule.nodes[0]["resname"]]
-        new_mol = block.to_molecule()
-        # we store the block together with the residue node
-        meta_molecule.nodes[0]["graph"] = new_mol.copy()
-        # here we generate a residue dict which is a collection
-        # of all unique residue regardless of connectivity
-        # it is faster than making the complete residue graph
-        res_dict = collect_residues(block, attrs=('resid', 'resname'))
-        if len(res_dict) > 1:
-            self.expand_meta_graph(meta_molecule, block, 0)
-
+        # get a defined order for looping over the resiude graph
         node_keys = list(meta_molecule.nodes.keys())
         node_keys.sort()
+
+        # get the first node and convert it to molecule
+        start_node = node_keys[0]
+        new_mol = self.force_field.blocks[self.node_to_block[start_node]].to_molecule()
+
+        # in this case the node belongs to a fragment for which there is a
+        # multiresidue block
+        if "from_itp" in meta_molecule.nodes[start_node]:
+            # add all nodes of that fragment to added_fragment nodes
+            fragment_nodes = list(self.fragments[self.node_to_fragment[start_node]].nodes())
+            self.added_fragment_nodes += list(fragment_nodes)
+
+            # extract the nodes of this paticular residue and store a
+            # dummy correspndance
+            correspondence = {node:node for node in new_mol.nodes}
+            self.multiblock_correspondence.append({node:node for node in new_mol.nodes})
+            residue = _correspondence_to_residue(meta_molecule,
+                                                 new_mol,
+                                                 correspondence,
+                                                 node)
+            # add residue to meta_molecule node
+            meta_molecule.nodes[start_node]["graph"] = residue
+        else:
+            # we store the block together with the residue node
+            meta_molecule.nodes[start_node]["graph"] = new_mol.copy()
+
+        # now we loop over the rest of the nodes
         for node in node_keys[1:]:
-            resname = meta_molecule.nodes[node]["resname"]
 
-            if node + 1 in nx.get_node_attributes(new_mol, "resid").values():
-                continue
-            block = force_field.blocks[resname]
-            correspondence = new_mol.merge_molecule(block)
+            # in this case the node belongs to a fragment which has been added
+            # we only extract the residue belonging to this paticular node
+            if node in self.added_fragments:
+                fragment_id = self.node_to_fragment[node]
+                correspondence = self.multiblock_correspondence[fragment_id]
+            # in this case we have to add the node from the block definitions
+            else:
+                block = self.force_field.blocks[self.node_to_block[node]]
+                correspondence = new_mol.merge_molecule(block)
 
-            residue = nx.Graph()
-            for res_node in correspondence.values():
-                data = new_mol.nodes[res_node]
-                residue.add_node(res_node, **data)
-                for attribute, value in meta_molecule.nodes[node].items():
-                    # graph and seqID are specific attributes set by make residue
-                    # graph or the gen_seq tool, which we don't want to propagate.
-                    if attribute in ["graph", "seqID"]:
-                        continue
-                    residue.nodes[res_node][attribute] = value
-
+            # make the residue from the correspondence
+            residue = _correspondence_to_residue(meta_molecule,
+                                                 new_mol,
+                                                 correspondence,
+                                                 node)
+            # add residue to node
             meta_molecule.nodes[node]["graph"] = residue
 
-            # here we generate a residue dict which is a collection
-            # of all unique residue regardless of connectivity
-            # it is faster than making the complete residue graph
-            res_dict = collect_residues(block, attrs=('resid', 'resname'))
-            if len(res_dict) > 1:
-                self.expand_meta_graph(meta_molecule, block, node)
+            # in this case we just added a new multiblock residue so we store
+            # the correspondence as well as keep track of the nodes that are
+            # part of that fragment
+            if "from_itp" in meta_molecule.nodes[node] and node not in self.added_fragments:
+                fragment_nodes = list(self.fragments[self.node_to_fragment[start_node]].nodes())
+                self.added_fragment_nodes += list(fragment_nodes)
+                self.multiblock_correspondence.append(correspondence)
+
 
         return new_mol
 
@@ -155,6 +196,17 @@ class MapToMolecule(Processor):
         vermouth.molecule.Molecule
             Either the provided molecule, or a brand new one.
         """
+        # in a first step we match the residue names to blocks
+        # in the force-field. Residue names can also be part
+        # of a larger fragment stored as a block or refer to
+        # a block which consists of multiple residues. This
+        # gets entangled here
+        self.match_nodes_to_blocks(meta_molecule)
+        # next we check if all exclusions are the same and if
+        # not we adjust it such that the lowest exclusion number
+        # is used. ApplyLinks then generates those appropiately
+        tag_exclusions(self.node_to_block, self.force_field)
+        # now we add the blocks generating a new molecule
         new_molecule = self.add_blocks(meta_molecule)
         meta_molecule.molecule = new_molecule
         return meta_molecule
