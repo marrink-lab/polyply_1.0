9c539401aede743843ed521d0e51a224bd77e40d
fgrunewald <f.grunewald@rug.nl>
2021-03-30 15:46:15 +0200

    remove dependancy of links on node keys

--- polyply/src/apply_links.py
+++ polyply/src/apply_links.py
@@ -225,19 +225,20 @@ def match_link_and_residue_atoms(meta_molecule, link, link_to_resid):
     for node in link.nodes:
         meta_mol_key = link_to_resid[node]
         block = meta_molecule.nodes[meta_mol_key]["graph"]
+        resid = block.nodes[list(block.nodes)[0]]["resid"]
         attrs = link.nodes[node]
-        attrs.update({'resid': meta_mol_key + 1})
-        ignore = ['order', 'charge_group', 'replace']
+        #attrs.update({'resid': meta_mol_key + 1})
+        ignore = ['order', 'charge_group', 'replace', 'resid']
         matchs = list(find_atoms(block, ignore=ignore, **attrs))
 
         if len(matchs) == 1:
             link_to_mol[node] = matchs[0]
         elif len(matchs) == 0:
             msg = "Found no matchs for node {} in resiue {}. Cannot apply link."
-            raise MatchError(msg.format(node, attrs["resid"]))
+            raise MatchError(msg.format(node, resid))
         else:
             msg = "Found {} matches for node {} in resiue {}. Cannot apply link."
-            raise MatchError(msg.format(len(matchs), node, attrs["resid"]))
+            raise MatchError(msg.format(len(matchs), node, resid))
 
     return link_to_mol
 
@@ -278,7 +279,7 @@ def _resnames_match(resnames, allowed_resnames):
     Parameters
     ----------
     resnames: `abc.iterable`
-    allowed_resnames: `abc.iterable`
+3, 'CH2': 2    allowed_resnames: `abc.iterable`
     """
     for resname in resnames:
         if resname in allowed_resnames:
@@ -411,7 +412,7 @@ class ApplyLinks(Processor):
         molecule = meta_molecule.molecule
         force_field = meta_molecule.force_field
         resnames = set(nx.get_node_attributes(molecule, "resname").values())
-
+        print(meta_molecule.nodes(data=True))
         for link in tqdm(force_field.links):
             link_resnames = _get_link_resnames(link)
             if not _resnames_match(resnames, link_resnames):
@@ -430,8 +431,11 @@ class ApplyLinks(Processor):
                                              edge_match=_linktype_match)
             raw_matchs = GM.subgraph_isomorphisms_iter()
             for match in raw_matchs:
-                resids = match.keys()
-                orders = [ res_link.nodes[match[resid]]["order"] for resid in resids]
+                nodes = match.keys()
+                resids =[meta_molecule.nodes[node]["resid"] for node in nodes]
+                orders = [ res_link.nodes[match[node]]["order"] for node in nodes]
+                #print(match)
+                #print(resids, orders)
                 if _check_relative_order(resids, orders):
                     link_node_to_resid = _assign_link_resids(res_link, match)
                     try:
