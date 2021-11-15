ed0b486863bf7c62fba81a2cb34e6e02a2ce518b
fgrunewald <f.grunewald@rug.nl>
2021-11-15 15:44:54 +0100

    add logging to source file

--- polyply/tests/test_logging.py
+++ polyply/tests/test_logging.py
@@ -5,32 +5,9 @@ import polyply
 from vermouth.log_helpers import (StyleAdapter, BipolarFormatter,
                                   CountingHandler, TypeAdapter,)
 from polyply.tests.test_build_file_parser import test_molecule, test_system
+from polyply.src.logging import LOGGER, LOGLEVELS
 
-# Implement Logger
-LOGGER = TypeAdapter(logging.getLogger('polyply'))
-PRETTY_FORMATTER = logging.Formatter(fmt='{levelname:} - {type} - {message}',
-                                     style='{')
-DETAILED_FORMATTER = logging.Formatter(fmt='{levelname:} - {type} - {name} - {message}',
-                                       style='{')
-COUNTER = CountingHandler()
-
-# Control above what level message we want to count
-COUNTER.setLevel(logging.WARNING)
-
-CONSOLE_HANDLER = logging.StreamHandler()
-FORMATTER = BipolarFormatter(DETAILED_FORMATTER,
-                             PRETTY_FORMATTER,
-                             logging.DEBUG,
-                             logger=LOGGER)
-
-CONSOLE_HANDLER.setFormatter(FORMATTER)
-LOGGER.addHandler(CONSOLE_HANDLER)
-LOGGER.addHandler(COUNTER)
-
-LOGGER = StyleAdapter(LOGGER)
-
-loglevels = {0: logging.INFO, 1: logging.DEBUG, 2: 5}
-LOGGER.setLevel(loglevels[1])
+LOGGER.setLevel(LOGLEVELS[1])
 
 
 @pytest.mark.parametrize('_type, option, expected, warning, idxs', (
