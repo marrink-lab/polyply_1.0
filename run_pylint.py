832466f71e3380c60b0e46fd30a3c893c3246d35
fgrunewald <32294573+fgrunewald@users.noreply.github.com>
2021-12-09 13:20:32 +0100

    print from run_pyling
    Debug run_pyling

--- run_pylint.py
+++ run_pylint.py
@@ -31,5 +31,8 @@ if __name__ == '__main__':
     threshold = args.threshold
     run = lint.Run(remaining_args, do_exit=False)
     score = run.linter.stats['global_note'] # Yes this is a terrible name for the score
+    print("score", score)
+    print("threshold", threshold)
+    print(run.linter.msg_status)
     if score < threshold:
         sys.exit(run.linter.msg_status)
