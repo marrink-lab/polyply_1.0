7076be21edfd4a277a408ddd10494d7d1991f375
fgrunewald <32294573+fgrunewald@users.noreply.github.com>
2021-12-09 13:59:09 +0100

    Update run_pylint.py
    Apparently the new pylint version does not support the dict like query of link stats anymore. Making it an attribute seems to work.

--- run_pylint.py
+++ run_pylint.py
@@ -30,9 +30,6 @@ if __name__ == '__main__':
 
     threshold = args.threshold
     run = lint.Run(remaining_args, do_exit=False)
-    score = run.linter.stats['global_note'] # Yes this is a terrible name for the score
-    print("score", score)
-    print("threshold", threshold)
-    print(run.linter.msg_status)
+    score = run.linter.stats.global_note # Yes this is a terrible name for the score
     if score < threshold:
         sys.exit(run.linter.msg_status)
