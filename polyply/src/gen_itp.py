241b9851d11a1faae2de48a454c4ccfc90c732b5
fgrunewald <f.grunewald@rug.nl>
2021-12-14 20:58:58 +0100

    fix change in deferred file writing

--- polyply/src/gen_itp.py
+++ polyply/src/gen_itp.py
@@ -20,7 +20,13 @@ import networkx as nx
 import vermouth
 import vermouth.forcefield
 from vermouth.log_helpers import StyleAdapter, get_logger
-from vermouth.file_writer import open, DeferredFileWriter
+# patch to get rid of martinize dependency
+try:
+    from vermouth.file_writer import deferred_open
+except ImportError:
+    from vermouth.file_writer import open
+    deferred_open = open
+from vermouth.file_writer import DeferredFileWriter
 from vermouth.citation_parser import citation_formatter
 from polyply import (MetaMolecule, ApplyLinks, Monomer, MapToMolecule)
 from .load_library import load_library
