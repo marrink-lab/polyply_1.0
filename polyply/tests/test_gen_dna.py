2c372b35d695e27a09b84d076aa769e884729477
fgrunewald <f.grunewald@rug.nl>
2022-03-08 17:38:46 +0100

    fix test and add circular test-case

--- polyply/tests/test_gen_dna.py
+++ polyply/tests/test_gen_dna.py
@@ -19,15 +19,32 @@ import networkx as nx
 from polyply.src.meta_molecule import MetaMolecule
 from polyply.src.gen_dna import complement_dsDNA
 
-def test_complement_dsDNA():
+@pytest.mark.parametrize('extra_edges, termini, expect_ter', (
+    # regular DNA strand
+    (
+    [],
+    {0: "DC5", 5: "DG3"},
+    {6: "DG3", 11: "DC5"}
+    ),
+    # circular DNA strand
+    (
+    [(5, 0)],
+    {0: "DC", 5: "DG"},
+    {6: "DG", 11: "DC"}
+    )
+    ))
+def test_complement_dsDNA(extra_edges, termini, expect_ter):
     test_DNA_mol = MetaMolecule()
-    nodes = range(0, 5)
+    nodes = range(0, 6)
     test_DNA_mol.add_edges_from(zip(nodes[:-1], nodes[1:]))
-    resnames = {0: "DC5", 1: "DG", 2: "DT", 3: "DC", 4: "DA", 5: "DG3"}
+    test_DNA_mol.add_edges_from(extra_edges)
+    resnames = {1: "DG", 2: "DT", 3: "DC", 4: "DA"}
+    resnames.update(termini)
     nx.set_node_attributes(test_DNA_mol, resnames, "resname")
-    nx.set_node_attributes(test_DNA_mol, dict(zip(nodes, range(1, 6))), "resid")
+    nx.set_node_attributes(test_DNA_mol, dict(zip(nodes, range(1, 7))), "resid")
     complement_dsDNA(test_DNA_mol)
     new_resnames = nx.get_node_attributes(test_DNA_mol, "resname")
-    resnames.update({6: "DG5", 7: "DC", 2: "DA", 8: "DG", 9: "DT", 10: "DC3"})
+    resnames.update({7: "DC", 8: "DA", 9: "DG", 10: "DT"})
+    resnames.update(expect_ter)
     assert new_resnames == resnames
     assert len(list(nx.connected_components(test_DNA_mol))) == 2
