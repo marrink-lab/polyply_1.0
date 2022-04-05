509103bb070f0fe507a0142cf0b7816a5dbc4f68
fgrunewald <f.grunewald@rug.nl>
2022-04-05 17:07:54 +0200

    implement the proper DNA convention when completing strands from sequence file; also fix cyclic sequences

--- polyply/src/gen_dna.py
+++ polyply/src/gen_dna.py
@@ -12,11 +12,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import networkx as nx
+from tqdm import tqdm
 
 BASE_LIBRARY = {"DA": "DT", "DT": "DA", "DG": "DC", "DC": "DG",
                 "DA5": "DT3", "DT5": "DA3", "DG5": "DC3", "DC5": "DG3",
                 "DA3": "DT5", "DT3": "DA5", "DG3": "DC5", "DC3": "DG5"
                 }
+
+def _dna_edge_iterator(meta_molecule, source):
+    """
+    Traversal method for DNA specifc meta-molecule.
+    Should become obsolete once vermouth commits to
+    directed graphs.
+    """
+    first_node = source
+    count = 0
+    while True:
+        neighbors = meta_molecule.neighbors(source)
+        src_resid = meta_molecule.nodes[source]["resid"]
+        count += 1
+        for next_node in neighbors:
+            next_resid = meta_molecule.nodes[next_node]["resid"]
+            diff = src_resid - next_resid
+            if diff == 1 :
+                yield (source, next_node)
+                source = next_node
+                break
+
+            if next_resid > src_resid and next_node == first_node:
+                yield (source, next_node)
+                return
+
+            if count > len(meta_molecule.nodes):
+                return
 
 def complement_dsDNA(meta_molecule):
     """
@@ -41,31 +69,40 @@ def complement_dsDNA(meta_molecule):
     """
     last_node = list(meta_molecule.nodes)[-1]
     resname = BASE_LIBRARY[meta_molecule.nodes[last_node]["resname"]]
-    meta_molecule.add_monomer(last_node+1, resname, [])
+    meta_molecule.add_monomer(last_node+1, resname, [], last_node+2)
 
     correspondance = {last_node: last_node+1}
     total = last_node+1
-    for prev_node, next_node in nx.dfs_edges(meta_molecule, source=last_node):
+
+    pbar = tqdm(total=len(meta_molecule.nodes))
+    for prev_node, next_node in _dna_edge_iterator(meta_molecule, source=last_node):
         try:
             resname = BASE_LIBRARY[meta_molecule.nodes[next_node]["resname"]]
         except KeyError:
             msg = ("Trying to complete a dsDNA strand. However, resname { } with resid {} "
-                   "does not match any of the known base-pair resnames. Note that polyply "
-                   "at the moment only allows base-pair completion for molecules that only "
-                   "consist of dsDNA. Please conact the developers if you whish to create a "
-                   "more complicated molecule.")
+                    "does not match any of the known base-pair resnames. Note that polyply "
+                    "at the moment only allows base-pair completion for molecules that only "
+                    "consist of dsDNA. Please conact the developers if you whish to create a "
+                    "more complicated molecule.")
 
             resname = meta_molecule.nodes[next_node]["resname"]
             resid = meta_molecule.nodes[next_node]["resid"]
             raise IOError(msg.format(resname, resid))
 
-        meta_molecule.add_monomer(total+1, resname, [(correspondance[prev_node], total+1)])
+        if next_node not in correspondance:
+            new_node = total + 1
+            meta_molecule.add_monomer(total+1, resname, [(correspondance[prev_node], total+1)], resid=total+2)
+            correspondance[next_node] = new_node
+        else:
+            new_node = correspondance[next_node]
+            meta_molecule.add_edge(correspondance[prev_node], new_node)
+
         # make sure that edge attributes are carried over
         for attr, value in meta_molecule.edges[(prev_node, next_node)].items():
-            meta_molecule.edges[(correspondance[prev_node], total+1)][attr] = value
+            meta_molecule.edges[(correspondance[prev_node], new_node)][attr] = value
 
-        correspondance[next_node] = total + 1
         total += 1
-
+        pbar.update(1)
+    pbar.close()
     return meta_molecule
 
