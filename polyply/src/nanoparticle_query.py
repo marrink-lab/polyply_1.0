5c9e8c5770e969ebb7d085ad7b6abcbdd578b98d
MDSYN2019 <sangyoung123@googlemail.com>
2023-10-15 20:38:36 +0100

    added python 2 function

--- polyply/src/nanoparticle_query.py
+++ polyply/src/nanoparticle_query.py
@@ -1,6 +1,32 @@
 import argparse
+import subprocess
+import logging
 from nanoparticle_lattice import NanoparticleModels, return_cg_nps_type
 from vermouth.gmx import gro
+
+PYTHON_II_PATH = "/home/sang/Python-2.7.9"
+INSANE_SCRIPT_PATH = (
+    "~/Desktop/Papers_NP/Personal_papers/polyply_paper/M3-Sterol-Parameters"
+)
+
+
+def generate_bilayer_process_insane(np_string: str = None) -> None:
+    """ """
+    command = f"{PYTHON_II_PATH}/python {INSANE_SCRIPT_PATH}/insane.py -l DPPC:4 -l DIPC:3 -l CHOL:3 -salt 0.15 -x 15 -y 10 -z 9 -d 0 -pbc cubic -sol W -o dppc-dipc-chol-insane.gro >> output.txt"
+
+    if np_string:
+        command = f"{PYTHON_II_PATH}/python {INSANE_SCRIPT_PATH}/insane.py -l DPPC:4 -l DIPC:3 -l CHOL:3 -salt 0.15 -x 15 -y 10 -z 9 -d 0 -pbc cubic -sol W -f {np_string} -o dppc-dipc-chol-insane.gro >> output.txt"
+
+    result = subprocess.run(
+        command,
+        shell=True,
+        universal_newlines=True,
+    )
+    if result.returncode == 0:
+        print("Python 2 script ran successfully.")
+        print(f"{result.stdout}")
+    else:
+        print("Python 2 script encountered an error.")
 
 
 def generate_pcbm_nanoparticle(input_gro, output_dir):
@@ -37,27 +63,28 @@ def generate_pcbm_nanoparticle(input_gro, output_dir):
     PCBM_model.write_itp(f"{output_dir}/PCBM.itp")
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Generate PCBM nanoparticle.")
-
-    # Define command-line arguments
-    parser.add_argument(
-        "--input-gro",
-        required=True,
-        help="Path to the input GRO file for PCBM_ligand.",
-    )
-
-    parser.add_argument(
-        "--output-dir",
-        required=True,
-        help="Path to the output directory for generated files.",
-    )
-
-    args = parser.parse_args()
-
-    # Call the function to generate the PCBM nanoparticle
-    generate_pcbm_nanoparticle(args.input_gro, args.output_dir)
+# def main():
+#    parser = argparse.ArgumentParser(description="Generate PCBM nanoparticle.")
+#
+#    # Define command-line arguments
+#    parser.add_argument(
+#        "--input-gro",
+#        required=True,
+#        help="Path to the input GRO file for PCBM_ligand.",
+#    )
+#
+#    parser.add_argument(
+#        "--output-dir",
+#        required=True,
+#        help="Path to the output directory for generated files.",
+#    )
+#
+#    args = parser.parse_args()
+#
+#    # Call the function to generate the PCBM nanoparticle
+#    generate_pcbm_nanoparticle(args.input_gro, args.output_dir)
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    generate_bilayer_process_insane(None)
