7c92c5085a67f31ffbb6c7f476d44005fc1c37d3
MDSYN2019 <sangyoung123@googlemail.com>
2024-05-04 22:16:25 +0100

    fix: made changes

--- polyply/src/ART_simulation/prepare_and_run_sim.sh
+++
@@ -1,6 +0,0 @@
-~/Desktop/gromacs-2022.5/build/bin/gmx grompp -f minimization.mdp -c ART.gro -p topol.top -o minim.tpr -maxwarn 10
-
-~/Desktop/gromacs-2022.5/build/bin/gmx mdrun -deffnm minim -v -c minimized.gro
-
-
-
