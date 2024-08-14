2aef8780431be16ca5f348ae49e6e22145ca2828
Fabian Gruenewald <f.grunewald@rug.nl>
2024-08-14 16:23:58 +0200

    put proper license in tests

--- polyply/tests/test_ffoutput.py
+++ polyply/tests/test_ffoutput.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Dr. Fabian Gruenewald
+#
+# Licensed under the PolyForm Noncommercial License 1.0.0;
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://polyformproject.org/licenses/noncommercial/1.0.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from pathlib import Path
 import pytest
 import vermouth
