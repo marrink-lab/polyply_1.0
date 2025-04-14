306afdba67f55ed589c34019e88ee1e7bdbf0656
jan-stevens <stevens.jan.adriaan@gmail.com>
2025-04-14 18:25:44 +0200

    improved oxdna parser for the 3'->5' convention in oxDNA

--- polyply/src/oxdna_parser.py
+++ polyply/src/oxdna_parser.py
@@ -4,7 +4,7 @@ from vermouth.molecule import Molecule
 
 OXDNA_TO_NM = 0.8518
 
-def read_oxdna(file_name, exclude=()):
+def read_oxdna(file_name, exclude=(), strand_lengths=None):
     """
     Parse an oxDNA configuration file to create a molecule.
 
@@ -15,13 +15,19 @@ def read_oxdna(file_name, exclude=()):
     exclude: collections.abc.Container[str]
         Atoms that have one of these residue names will not be included.
 
+    strand_lengths: list of int or None
+        The lengths of each DNA strand. If None, the function will attempt to
+        guess based on the total number of nucleotides (assuming equal strands
+        for even numbers). For example, [12, 12] would indicate two strands of
+        12 nucleotides each.
+
     Returns
     -------
     vermouth.molecule.Molecule
         The parsed molecule. Will not contain edges.
     """
     molecule = Molecule()
-    idx = 0
+    nucleotides = []
 
     with open(str(file_name)) as oxdna:
         # Parse header
@@ -56,18 +62,45 @@ def read_oxdna(file_name, exclude=()):
 
             # Construct major groove vector
             major_groove_vector = np.cross(base_normal_vector, base_vector)
-            # major_groove_vector = np.cross(base_vector, base_normal_vector)
+            major_groove_vector /= np.linalg.norm(major_groove_vector)
 
             # Store the orientation frame
-            # orientation_frame = np.column_stack([base_vector, major_groove_vector, base_normal_vector])
-            orientation_frame = np.column_stack([major_groove_vector, -base_vector, base_normal_vector])
+            orientation_frame = np.column_stack([base_vector, major_groove_vector, base_normal_vector])
+
             properties['orientation'] = orientation_frame
 
-            # Default values
-            properties['resid'] = idx + 1
+            # Add to nucleotides list
+            nucleotides.append(properties)
 
-            molecule.add_node(idx, **properties)
-            idx += 1
+    # If strand_lengths is not provided, try to guess based on the number of nucleotides
+    if strand_lengths is None:
+        # If the number of nucleotides is even, assume it's a duplex with equal strand lengths
+        if len(nucleotides) % 2 == 0:
+            strand_lengths = [len(nucleotides) // 2, len(nucleotides) // 2]
+        else:
+            raise IOError("We couldn't infer the duplex strands in the system.")
+
+    # Process nucleotides based on strand_lengths and convert 3' to 5' to 5' to 3'
+    processed_nucleotides = []
+
+    # Process each strand separately
+    start_idx = 0
+    for length in strand_lengths:
+        end_idx = start_idx + length
+        strand = nucleotides[start_idx:end_idx]
+
+        strand = list(reversed(strand))
+
+        processed_nucleotides.extend(strand)
+        start_idx = end_idx
+
+    # Add nucleotides to the molecule
+    for idx, properties in enumerate(processed_nucleotides):
+        # Set resid
+        properties['resid'] = idx + 1
+
+        # Add node to molecule
+        molecule.add_node(idx, **properties)
 
     # Set box
     molecule.box = box
